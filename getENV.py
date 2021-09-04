9978376e1a229b25d1e541fabdb543e4cf01e3a2
Leon <LeoninLyon@gmail.com>
2021-09-04 13:15:02 +0800

    🔨完善错误说明

--- getENV.py
+++ getENV.py
@@ -8,7 +8,7 @@ def getdata():
     ql_config_file = '/ql/config/check.json'
     print('尝试检查环境\n')
     if os.path.exists(v2p_new):
-        print('成功 当前环境为 v2p 面板继续执行')
+        print('成功，当前环境为 v2p 面板继续执行')
         if os.path.exists(v2p_config_file):
             print('已找到配置文件')
             try:
@@ -16,14 +16,13 @@ def getdata():
                     data = json.loads(f.read())
                 return data
             except json.JSONDecodeError:
-                print('错误！请在 https://www.json.cn/json/jsononline.html 中检查 JSON 格式')
+                print('错误！请复制整个 check.json 内容到 https://www.json.cn/json/jsononline.html 中检查 JSON 格式')
                 exit(1)
         else:
-            print('未找到配置文件\n')
-            print('请添加 ./script/Shell/check.json')
+            print('错误！未找到配置文件\n请添加 ./script/Shell/check.json')
             exit(1)
     elif os.path.exists(ql_new):
-        print('成功 当前环境为青龙面板继续执行')
+        print('成功，当前环境为青龙面板继续执行')
         if os.path.exists(ql_config_file):
             print('已找到配置文件')
             try:
@@ -31,12 +30,11 @@ def getdata():
                     data = json.loads(f.read())
                 return data
             except json.JSONDecodeError:
-                print('错误！请在 https://www.json.cn/json/jsononline.html 中检查 JSON 格式')
+                print('错误！请复制整个 check.json 内容到 https://www.json.cn/json/jsononline.html 中检查 JSON 格式')
                 exit(1)            
         else:
-            print('未找到配置文件\n')
-            print('请添加 /ql/config/check.json')
+            print('错误！未找到配置文件\n请添加 /ql/config/check.json')
             exit(1)
     else:
-        print('失败 请检查环境')
+        print('失败！请检查环境')
         exit(0)
