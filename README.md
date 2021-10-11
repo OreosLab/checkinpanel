6b7d31c1e78bf4823ad8e7180ba25d09184d6d78
Oreomeow <85870818+Oreomeow@users.noreply.github.com>
2021-10-11 19:09:17 +0800

    :sparkles: 新增 SF 轻小说

--- README.md
+++ README.md
@@ -271,7 +271,7 @@ cp /ql/repo/Oreomeow_checkinpanel_master/notify.json5 /ql/config/notify.json5
 | --- | --- | --- | --- |
 * [x] 𝑷𝒚𝒕𝒉𝒐𝒏 | api | LeetCode 每日一题 | 每日一句 | 天气预报
 * [x] 𝑷𝒚𝒕𝒉𝒐𝒏 | 多账号 | AcFun | 百度搜索资源平台 | Bilibili | 天翼云盘 | CSDN | 多看阅读 | 恩山论坛 | Fa米家 | 网易云游戏 | 葫芦侠 | 爱奇艺 | 全民K歌 | MEIZU 社区 | 芒果 TV | 小米运动 | 网易云音乐 | 一加手机社区官方论坛 | 哔咔漫画 | 吾爱破解 | 什么值得买 | 百度贴吧 | V2EX | 腾讯视频 | 微博 | 联通沃邮箱 | 哔咔网单 | 王者营地 | 有道云笔记 | 智友邦
-* [x] 𝑷𝒚𝒕𝒉𝒐𝒏 | 多账号 | 机场签到 | 欢太商城 | NGA | 掘金 | GLaDOS | HiFiNi | 时光相册 | 联通营业厅 | 无忧行 | FreeNom | EUserv | Site 
+* [x] 𝑷𝒚𝒕𝒉𝒐𝒏 | 多账号 | 机场签到 | 欢太商城 | NGA | 掘金 | GLaDOS | HiFiNi | 时光相册 | 联通营业厅 | 无忧行 | FreeNom | EUserv | Site | SF 轻小说
 * [x] 𝑺𝒉𝒆𝒍𝒍 | 多账号 | SSPanel 签到
 
 ### 项目完成情况
@@ -295,7 +295,7 @@ cp /ql/repo/Oreomeow_checkinpanel_master/notify.json5 /ql/config/notify.json5
 | 状态 | 名称 |
 | --- | --- |
 | ✅ | LeetCode 每日一题 \| 每日一句 \| 天气预报 \| AcFun \| 机场签到 \| Bilibili \| 天翼云盘 \| CSDN \| 多看阅读 \| 恩山论坛 \| 时光相册 \| FreeNom \| GLaDOS \| 网易云游戏 \| 欢太商城 \| HiFiNi \| 爱奇艺 \| 掘金 \| 全民K歌 \| MEIZU 社区 \| 小米运动 \| 网易云音乐 \| NGA \| 一加手机社区官方论坛 \| 吾爱破解 \| 什么值得买 \| SSPanel 签到 \| 百度贴吧 \| 腾讯视频 \| 微博 \| 王者营地 \| 有道云笔记 |
-| ❔ | 百度搜索资源平台 \| EUserv \| Fa米家 \| 葫芦侠 \| 无忧行 \| 芒果 TV \| 哔咔漫画 \| Site \| 联通营业厅 \| V2EX \| 联通沃邮箱 \| 哔咔网单 \| 智友邦 |
+| ❔ | 百度搜索资源平台 \| EUserv \| Fa米家 \| 葫芦侠 \| 无忧行 \| 芒果 TV \| 哔咔漫画 \| SF 轻小说 \| Site \| 联通营业厅 \| V2EX \| 联通沃邮箱 \| 哔咔网单 \| 智友邦 |
 
 ## 致谢
 
---
+++ ck_sfacg.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+cron: 15 10 * * *
+new Env('SF 轻小说');
+"""
+
+import json
+import time
+from datetime import datetime, timedelta, timezone
+
+import requests
+
+from notify_mtr import send
+from utils import get_data
+
+utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
+bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
+timestamp = bj_dt.timestamp()
+readingDate = datetime.fromtimestamp(timestamp).strftime("%Y 年 %m 月 %d 日")
+
+
+class SFACG:
+    def __init__(self, check_items):
+        self.check_items = check_items
+
+    @staticmethod
+    def generateHeader(authorization, cookie, useragent, sfsecurity):
+        headers = {
+            "Host": "api.sfacg.com",
+            "accept-charset": "UTF-8",
+            "accept": "application/vnd.sfacg.api+json;version=1",
+            "authorization": authorization,
+            "cookie": cookie,
+            "user-agent": useragent,
+            "sfsecurity": sfsecurity.split("&")[0] + "&timestamp=" + str(int(timestamp)) + "&" + sfsecurity.split("&")[2] + "&" + sfsecurity.split("&")[3],
+            "accept-encoding": "gzip"
+        }
+        return headers
+
+    @staticmethod
+    def post_re(api, headers, data):
+        return requests.post(api, headers=headers, data=data).json()
+
+    @staticmethod
+    def get_re(api, headers):
+        return requests.get(url=api, headers=headers).json()
+
+    @staticmethod
+    def put_re(api, put_headers, data):
+        return requests.put(api, headers=put_headers, data=data).json()
+
+    def check_cookie(self, authorization, cookie, useragent, sfsecurity):
+        headers = self.generateHeader(authorization, cookie, useragent, sfsecurity)
+        result = requests.get("https://api.sfacg.com/user?", headers=headers).json()
+        money = requests.get("https://api.sfacg.com/user/money", headers=headers).json()
+        try:
+            nick_Name = result["data"]["nickName"]
+            fireMoneyRemain = money["data"]["fireMoneyRemain"]
+            user_vipLevel = money["data"]["vipLevel"]
+            info = "账号名称: " + nick_Name + "\n火卷余额: " + str(fireMoneyRemain) + "\nVIP: " + str(user_vipLevel)
+            print("Cookie 凭证有效！", info)
+        except BaseException:
+            info = "Cookie 凭证失效 httpCode: " + str(result["status"]["httpCode"])
+            print(info)
+        return info
+
+    def task(self, authorization, cookie, useragent, sfsecurity):
+        headers = self.generateHeader(authorization, cookie, useragent, sfsecurity)
+        print("运行时间:", readingDate)
+        ReadTime = {
+            "seconds": 3605,
+            "readingDate": readingDate,
+            "entityType": 2
+        }
+        ListenTime = {
+            "seconds": 3605,
+            "readingDate": readingDate,
+            "entityType": 3
+        }
+        ReadData = json.dumps(ReadTime)
+        ListenData = json.dumps(ListenTime)
+
+        put_headers = headers
+        put_headers["accept-encoding"] = "gzip"
+        put_headers["content-length"] = "57"
+        put_headers["content-type"] = "application/json; charset=UTF-8"
+
+        print("开始执行任务")
+        self.put_re("https://api.sfacg.com/user/readingtime", put_headers, data=ListenData)
+        self.post_re("https://api.sfacg.com/user/tasks/4", headers, data=ListenData)
+        self.post_re("https://api.sfacg.com/user/tasks/5", headers, data=ListenData)
+        self.post_re("https://api.sfacg.com/user/tasks/17", headers, data=ListenData)
+        for i in range(3):
+            r = self.put_re("https://api.sfacg.com/user/readingtime", put_headers, ReadData)
+            print(r)
+            time.sleep(0.5)
+            self.put_re("https://api.sfacg.com/user/tasks/5", put_headers, data=ListenData)
+            self.put_re("https://api.sfacg.com/user/tasks/4", put_headers, data=ListenData)
+            self.put_re("https://api.sfacg.com/user/tasks/17", put_headers, data=ListenData)
+
+    def checkin(self, authorization, cookie, useragent, sfsecurity):
+        headers = self.generateHeader(authorization, cookie, useragent, sfsecurity)
+        print("运行时间:", readingDate)
+        sign_date = "{} 年 {} 月 {} 日"
+        for data in self.get_re("https://api.sfacg.com/user/signInfo", headers)["data"]:
+            sign_date = sign_date.format(data["year"], data["month"], data["day"])
+        if sign_date == readingDate:
+            sign_msg = "已签提醒: 您今天已经签过到了,请明天再来"
+            print(sign_msg)
+        else:
+            print("检测到今天还未签到，开始自动签到和完成任务")
+            response = requests.put("https://api.sfacg.com/user/signInfo", headers=headers).json()
+            sign_msg = "签到提醒: " + response["status"]["msg"]
+            for data in self.get_re("https://api.sfacg.com/user/signInfo", headers)["data"]:
+                sign_msg += "\n签到日期: " + \
+                    sign_date.format(data["year"], data["month"], data["day"]) + "，连续签到 " + data["continueNum"] + " 天"
+            self.task(authorization, cookie, useragent, sfsecurity)
+            print(sign_msg)
+        return sign_msg
+
+    def check_coin(self, authorization, cookie, useragent, sfsecurity):
+        headers = self.generateHeader(authorization, cookie, useragent, sfsecurity)
+        coin = self.get_re("https://api.sfacg.com/user/welfare/income", headers)["data"]["coinRemain"]
+        coin_info = "金币数量: " + str(coin)
+        return coin_info
+
+    def main(self):
+        msg_all = ""
+        for check_item in self.check_items:
+            authorization = check_item.get("authorization")
+            cookie = check_item.get("cookie")
+            useragent = check_item.get("useragent")
+            sfsecurity = check_item.get("sfsecurity")
+            info = self.check_cookie(authorization, cookie, useragent, sfsecurity)
+            sign_msg = self.checkin(authorization, cookie, useragent, sfsecurity)
+            coin_info = self.check_coin(authorization, cookie, useragent, sfsecurity)
+            msg = info + "\n" + sign_msg + "\n" + coin_info
+            msg_all += msg + "\n\n"
+        return msg_all
+
+
+if __name__ == "__main__":
+    data = get_data()
+    _check_items = data.get("SFACG", [])
+    res = SFACG(check_items=_check_items).main()
+    send("SFACG", res)
