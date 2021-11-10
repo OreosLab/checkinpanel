7efbc3c61e7ab1f26663bc2009c94997f3abf780
Oreo <62660101+Oreomeow@users.noreply.github.com>
2021-11-10 17:32:33 +0800

    :bug: 修复 str 与其他类型连接 bug

--- ck_youdao.py
+++ ck_youdao.py
@@ -65,7 +65,7 @@ class YouDao:
                 space = str(sync + checkin + checkin2 + ad)
                 msg += f"获得空间：{space}M, 总空间：{int(s)//1048576}M"
         else:
-            msg += "错误" + re.json()
+            msg += "错误" + str(re.json())
         return msg
 
     def main(self):
