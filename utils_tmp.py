a912594ea76cbecb4dae57da138f47ad3b8b18eb
Oreo <62660101+Oreomeow@users.noreply.github.com>
2021-11-14 18:31:29 +0800

    :sparkles: 同步更新 HeyTap

--- utils_tmp.py
+++ utils_tmp.py
@@ -1,120 +1,11 @@
-expired_act_list = [
-    {
-        "act_name": "818周年庆",
-        "aid": 1506,
-        "if_task": True,
-        "referer": "https://hd.oppo.com/act/m/2021/huantai618jifenpengz/index.html?us=shouye&um=youshangjiao&uc=zhounianqing",
-        "if_draw": False,  # 已修复抽奖，但貌似仍然不中奖？先设置为False，请自行测试，如不需要抽奖请自行修改为False
-        "extra_draw_cookie": 'app_innerutm={"uc":"zhounianqing","um":"youshangjiao","ut":"direct","us":"shouye"};app_utm={"utm_source_app":"direct","utm_campaign_app":"direct","utm_term_app":"direct","utm_medium_app":"direct"};',
-        "lid": 1516,
-        "draw_times": 3,
-        "end_time": "2021-8-18 23:59:59",
-        "text": "每次扣取5积分",
-    },
-    {
-        "act_name": "realme积分大乱斗1",
-        "aid": 1582,
-        "if_task": True,
-        "referer": "https://hd.oppo.com/act/m/2021/2021/realmejifendalu/index.html",
-        "if_draw": True,  # 已修复抽奖，如不需要抽奖请自行修改为False
-        "extra_draw_cookie": 'app_innerutm={"uc":"renwuzhongxin","um":"hudongleyuan","ut":"direct","us":"gerenzhongxin"};app_utm={"utm_source_app":"direct","utm_campaign_app":"direct","utm_term_app":"direct","utm_medium_app":"direct"};',
-        "lid": 1466,
-        "draw_times": 3,
-        "end_time": "2021-8-31 23:59:59",
-        "text": "每次扣取10积分",
-    },
-    {
-        "act_name": "realme积分大乱斗2",
-        "aid": 1598,
-        "if_task": True,
-        "referer": "https://hd.oppo.com/act/m/2021/huantaishangchengjif/index.html?&us=realmeshouye&um=icon&ut=3&uc=realmejifendaluandou",
-        "if_draw": True,  # 已修复抽奖，如不需要抽奖请自行修改为False
-        "extra_draw_cookie": 'app_innerutm={"uc":"realmejifendaluandou","um":"icon","ut":"3","us":"realmeshouye"};app_utm={"utm_source_app":"direct","utm_campaign_app":"direct","utm_term_app":"direct","utm_medium_app":"direct"};',
-        "lid": 1487,
-        "draw_times": 3,
-        "end_time": "2021-8-31 23:59:59",
-        "text": "每次扣取10积分",
-    },
-    {
-        "act_name": "realme宠粉计划1",
-        "aid": 1182,
-        "if_task": False,  # 该活动没有任务
-        "referer": "https://hd.oppo.com/act/m/2020/realmechannejifendui/index.html?us=relmechannel&um=huodongyaofeng&uc=realmechongfenchoujiang",
-        "if_draw": True,  # 已修复抽奖，如不需要抽奖请自行修改为False
-        "extra_draw_cookie": 'app_innerutm={"uc":"realmechongfenchoujiang","um":"huodongyaofeng","ut":"direct","us":"relmechannel"};app_utm={"utm_source_app":"direct","utm_campaign_app":"direct","utm_term_app":"direct","utm_medium_app":"direct"};',
-        "lid": 1347,
-        "draw_times": 1,
-        "end_time": "2021-8-31 23:59:59",
-        "text": "每次扣取10积分",
-    },
-    {
-        "act_name": "realme宠粉计划2",
-        "aid": 1616,
-        "if_task": False,  # 该活动没有任务
-        "referer": "https://hd.oppo.com/act/m/2020/realmechannejifendui/index.html?us=relmechannel&um=huodongyaofeng&uc=realmechongfenchoujiang",
-        "if_draw": True,  # 已修复抽奖，如不需要抽奖请自行修改为False
-        "extra_draw_cookie": 'app_innerutm={"uc":"realmechongfenchoujiang","um":"huodongyaofeng","ut":"direct","us":"relmechannel"};app_utm={"utm_source_app":"direct","utm_campaign_app":"direct","utm_term_app":"direct","utm_medium_app":"direct"};',
-        "lid": 1521,
-        "draw_times": 1,
-        "end_time": "2021-8-31 23:59:59",
-        "text": "每次扣取10积分",
-    },
-    {
-        "act_name": "智能硬件狂撒积分",
-        "aid": 1594,
-        "if_task": True,
-        "referer": "https://hd.oppo.com/act/m/2021/huantai618jifenpengz/index.html?us=shouye&um=youshangjiao&uc=zhounianqing",
-        "if_draw": False,  # 该活动中奖率奇低，请自行选择是否抽奖
-        "extra_draw_cookie": 'app_innerutm={"uc":"renwuzhongxin","um":"hudongleyuan","ut":"direct","us":"gerenzhongxin"};app_utm={"utm_source_app":"direct","utm_campaign_app":"direct","utm_term_app":"direct","utm_medium_app":"direct"};',
-        "lid": 1482,
-        "draw_times": 3,
-        "end_time": "2021-8-18 23:59:59",  # 8-8过期，预计过期还能玩，延期几天
-        "text": "每次扣取10积分",
-    },
-    {
-        "act_name": "天天积分翻倍",
-        "aid": 675,
-        "if_task": False,  # 该活动没有任务
-        "referer": "https://hd.oppo.com/act/m/2019/jifenfanbei/index.html?us=qiandao&um=task",
-        "if_draw": True,  # 已修复抽奖，如不需要抽奖请自行修改为False
-        "extra_draw_cookie": 'app_innerutm={"uc":"direct","um":"zuoshangjiao","ut":"direct","us":"shouye"};app_utm={"utm_source_app":"direct","utm_campaign_app":"direct","utm_term_app":"direct","utm_medium_app":"direct"};',
-        "lid": 1289,
-        "draw_times": 3,
-        "end_time": "2033-8-18 23:59:59",  # 长期任务
-        "text": "每次扣取10积分",
-    },
-    {
-        "act_name": "智能生活",
-        "aid": 1270,  # 从原作者代码提取，但是没找到活动页面在哪，暂不提供抽奖
-        "if_task": False,  # 该活动没有任务
-        "referer": "https://hd.oppo.com/act/m/2019/jifenfanbei/index.html?us=qiandao&um=task",
-        "if_draw": False,  # 暂时关闭抽奖，请手动抽奖
-        "extra_draw_cookie": "",
-        "lid": 1431,
-        "draw_times": 5,
-        "end_time": "2021-8-18 23:59:59",
-    },
-    {
-        "act_name": "智能硬件0元抽奖",
-        "aid": 1588,
-        "if_task": False,  # 该活动没有任务
-        "referer": "https://hd.oppo.com/act/m/2021/3719/index.html?us=iotchannel&um=icon",
-        "if_draw": True,  # 已修复抽奖，如不需要抽奖请自行修改为False
-        "extra_draw_cookie": 'app_innerutm={"uc":"direct","um":"icon","ut":"direct","us":"iotchannel"};app_utm={"utm_source_app":"direct","utm_campaign_app":"direct","utm_term_app":"direct","utm_medium_app":"direct"};',
-        "lid": 1514,
-        "draw_times": 3,
-        "end_time": "2021-8-18 23:59:59",  # 活动8-5结束，实际还能玩
-        "text": "每次扣取0积分",
-    },
-]
-
+# -*- coding: utf8 -*-
 act_list = [
     {
         "act_name": "赚积分",
         "aid": 1418,
         "if_task": True,  # 是否有任务
         "referer": "https://hd.oppo.com/act/m/2021/jifenzhuanpan/index.html?us=gerenzhongxin&um=hudongleyuan&uc=yingjifen",
-        "if_draw": False,  # 是否有抽奖活动，已修复抽奖，如不需要抽奖请自行修改为False
+        "if_draw": False,  # 是否有抽奖活动，不推荐开启抽奖，建议手动
         "extra_draw_cookie": 'app_innerutm={"uc":"yingjifen","um":"hudongleyuan","ut":"direct","us":"gerenzhongxin"};',
         # 抽奖必要的额外cookie信息，请勿随意修改，否则可能导致不中奖
         "lid": 1307,  # 抽奖参数
@@ -127,7 +18,7 @@ act_list = [
         "aid": 1582,
         "if_task": True,
         "referer": "https://hd.oppo.com/act/m/2021/2021/realmejifendalu/index.html",
-        "if_draw": False,  # 已修复抽奖，如不需要抽奖请自行修改为False
+        "if_draw": False,  # 不推荐开启抽奖，建议手动
         "extra_draw_cookie": 'app_innerutm={"uc":"renwuzhongxin","um":"hudongleyuan","ut":"direct","us":"gerenzhongxin"};',
         "lid": 1466,
         "draw_times": 3,
@@ -139,7 +30,7 @@ act_list = [
         "aid": 1582,
         "if_task": True,
         "referer": "https://hd.oppo.com/act/m/2021/2021/realmejifendalu/index.html?&us=realmenewshouye&um=yaofen&ut=right&uc=realmedaluandou",
-        "if_draw": False,  # 已修复抽奖，如不需要抽奖请自行修改为False
+        "if_draw": False,  # 不推荐开启抽奖，建议手动
         "extra_draw_cookie": 'app_innerutm={"uc":"renwuzhongxin","um":"hudongleyuan","ut":"direct","us":"gerenzhongxin"};',
         "lid": 1554,  # 抽奖接口与8月不一样，测试可以独立抽奖
         "draw_times": 3,
@@ -151,7 +42,7 @@ act_list = [
         "aid": 1598,
         "if_task": True,
         "referer": "https://hd.oppo.com/act/m/2021/huantaishangchengjif/index.html?&us=realmeshouye&um=icon&ut=3&uc=realmejifendaluandou",
-        "if_draw": False,  # 已修复抽奖，如不需要抽奖请自行修改为False
+        "if_draw": False,  # 不推荐开启抽奖，建议手动
         "extra_draw_cookie": 'app_innerutm={"uc":"realmejifendaluandou","um":"icon","ut":"3","us":"realmeshouye"};',
         "lid": 1535,
         "draw_times": 3,
@@ -163,7 +54,7 @@ act_list = [
         "aid": 675,
         "if_task": False,  # 该活动没有任务
         "referer": "https://hd.oppo.com/act/m/2019/jifenfanbei/index.html?us=qiandao&um=task",
-        "if_draw": False,  # 已修复抽奖，如不需要抽奖请自行修改为False
+        "if_draw": False,  # 不推荐开启抽奖，建议手动
         "extra_draw_cookie": 'app_innerutm={"uc":"direct","um":"zuoshangjiao","ut":"direct","us":"shouye"};',
         "lid": 1289,
         "draw_times": 1,
@@ -171,28 +62,40 @@ act_list = [
         "text": "每次扣取10积分",
     },
     {
-        "act_name": "智能硬件0元抽奖",
-        "aid": 1588,
-        "if_task": False,  # 该活动没有任务
-        "referer": "https://hd.oppo.com/act/m/2021/3719/index.html?us=iotchannel&um=icon",
-        "if_draw": False,  # 已修复抽奖，如不需要抽奖请自行修改为False
-        "extra_draw_cookie": 'app_innerutm={"uc":"direct","um":"icon","ut":"direct","us":"iotchannel"};',
-        "lid": 1514,
-        "draw_times": 3,
-        "end_time": "2022-8-18 23:59:59",  # 活动8-5结束，实际还能玩
-        "text": "每次扣取0积分",
-    },
-    {
         "act_name": "双十一活动",
         "aid": 1768,
         "if_task": True,
         "referer": "https://hd.oppo.com/act/m/2021/choumiandan/index.html?us=shouye&um=youshangjiao&uc=2021oppowin",
-        "if_draw": False,  # 已修复抽奖，如不需要抽奖请自行修改为False
+        "if_draw": False,  # 不推荐开启抽奖，建议手动
         "extra_draw_cookie": 'app_innerutm={"uc":"direct","um":"direct","ut":"direct","us":"direct"};',
         "lid": 1586,
         "draw_times": 1,
-        "end_time": "2021-11-13 23:59:59",  # 长期任务
+        "end_time": "2021-11-13 23:59:59",
         "text": "每次扣取5积分",
+    },
+    {
+        "act_name": "OPPO会员日",
+        "aid": 1825,
+        "if_task": True,
+        "referer": "https://hd.oppo.com/act/m/2021/3719/index.html?us=iotchannel&um=icon",
+        "if_draw": False,  # 不推荐开启抽奖，建议手动
+        "extra_draw_cookie": 'app_innerutm={"uc":"direct","um":"direct","ut":"direct","us":"direct"};',
+        "lid": 1606,
+        "draw_times": 0,
+        "end_time": "2021-11-16 23:59:59",
+        "text": "每次扣取5积分",
+    },
+    {
+        "act_name": "一加加油站",
+        "aid": 1473,
+        "if_task": True,
+        "referer": "https://hd.oppo.com/act/m/2021/OnePlusJYStation/index.html?us=onepluschannel&um=active",
+        "if_draw": False,  # 不推荐开启抽奖，建议手动
+        "extra_draw_cookie": 'app_innerutm={"uc":"direct","um":"direct","ut":"direct","us":"direct"};',
+        "lid": 1606,
+        "draw_times": 0,
+        "end_time": "2022-11-16 23:59:59",  # 没写结束时间，先设置长期
+        "text": "每次扣取0积分",
     },
 ]
 
--- utils_ver.py
+++ utils_ver.py
@@ -5,7 +5,7 @@ import time
 
 import requests
 
-__version__ = "20211114-3-072"
+__version__ = "20211114-3-172"
 ONLINE_VERSION = ""
 ver_re = re.compile("__version__ = .(\\d+-\\d+-...).")
 
