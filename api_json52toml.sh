ebfd19a5de67e9dd06964b231802a074005258e6
Oreomeow <85870818+Oreomeow@users.noreply.github.com>
2021-11-17 22:37:12 +0800

    :racehorse: 分离 JSON5 to TOML 和签到依赖代码

---
+++ api_json52toml.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2188
+<<'COMMENT'
+cron: 45 0-23/1 * * *
+new Env('JSON5toTOML 工具');
+COMMENT
+
+json52toml() {
+    if [[ -f /ql/config/config.sh ]]; then
+        sed -i '/^RepoFileExtensions/c RepoFileExtensions="js pl py sh ts"' /ql/config/config.sh
+        ql repo https://github.com/Oreomeow/checkinpanel.git "api_|ck_|ins_" "^checkin" "^notify|^utils" "master"
+        cp -f /ql/repo/Oreomeow_checkinpanel_master/check.sample.toml /ql/config/check.template.toml
+    fi
+    find . -type f -name '*utils_json52toml.pl' -exec perl {} \;
+}
+json52toml
--- utils_ver.py
+++ utils_ver.py
@@ -5,7 +5,7 @@ import time
 
 import requests
 
-__version__ = "20211117-3-3c5"
+__version__ = "20211117-3-4c5"
 ONLINE_VERSION = ""
 ver_re = re.compile("__version__ = .(\\d+-\\d+-...).")
 
