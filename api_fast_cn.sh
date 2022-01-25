a72608780747a0693ca5b0a27b206544405d5540
Oreomeow <85870818+Oreomeow@users.noreply.github.com>
2022-01-25 22:29:52 +0800

    :sparkles: 恢复更新
    1. 同步更新 epic
    2. 修复 iqiyi
    3. 整合新脚本：换源、RSS

--- api_fast_cn.sh
+++ api_fast_cn.sh
@@ -6,27 +6,24 @@ cron: 45 12 */7 * *
 new Env('国内加速');
 COMMENT
 
-
-# alpine换源
+# alpine 换源
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
 
-# Python换源
+# Python 换源
 mkdir -p /root/.config/pip
-cat>/root/.config/pip/pip.conf<<EOF
+cat >/root/.config/pip/pip.conf <<EOF
 [global]                                                                                                                             
 index-url=https://pypi.mirrors.ustc.edu.cn/simple/
 EOF
 
-
-# NPM换源
+# NPM 换源
 npm config set registry https://registry.npm.taobao.org/
 
-
-# CPAN换源
+# CPAN 换源
 if ! (
     perl -MCPAN -e 'CPAN::HandleConfig->load();' \
         -e 'CPAN::HandleConfig->prettyprint("urllist")' |
-    grep -qF 'https://mirrors.tuna.tsinghua.edu.cn/CPAN/'
+        grep -qF 'https://mirrors.tuna.tsinghua.edu.cn/CPAN/'
 ); then
     perl -MCPAN -e 'CPAN::HandleConfig->load();' \
         -e 'CPAN::HandleConfig->edit("urllist", "unshift", "https://mirrors.tuna.tsinghua.edu.cn/CPAN/");' \
--- api_rssbot.py
+++ api_rssbot.py
@@ -4,21 +4,25 @@ cron: */15 6-22 * * *
 new Env('RSS 订阅推送');
 """
 
+from datetime import datetime, timedelta
+
 import feedparser
 
-from datetime import datetime, timedelta
-from models import db, Rss, History
 from notify_mtr import send
+from utils_models import History, Rss, db
 
 
 class RssRobot:
-
     def main(self):
         self.remove_old_history()
         rss_list = Rss.select()
         msg = ""
-        post_url_list = [rss_history.url for rss_history in
-                         History.select().where(History.publish_at == datetime.today().strftime("%Y-%m-%d"))]
+        post_url_list = [
+            rss_history.url
+            for rss_history in History.select().where(
+                History.publish_at == datetime.today().strftime("%Y-%m-%d")
+            )
+        ]
         for rss in rss_list:
             rss_history_list = []
             feed = feedparser.parse(rss.feed)
@@ -35,10 +39,11 @@ class RssRobot:
     def remove_old_history(self):
         # 只保留最近一周的记录
         week_date_range = datetime.now() + timedelta(days=-7)
-        History.delete().where(History.publish_at <
-                               week_date_range.strftime("%Y-%m-%d")).execute()
+        History.delete().where(
+            History.publish_at < week_date_range.strftime("%Y-%m-%d")
+        ).execute()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     res = RssRobot().main()
     send("RSS 订阅", res)
