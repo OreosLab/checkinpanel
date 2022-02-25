cc4383bf774badcc5f622d728c9913479203b697
Oreomeow <85870818+Oreomeow@users.noreply.github.com>
2022-02-26 00:40:13 +0800

    :zap: 分批发送：rssbot

--- api_rssbot.py
+++ api_rssbot.py
@@ -15,14 +15,17 @@ from utils_models import History, Rss, db
 class RssRobot:
     def main(self):
         self.remove_old_history()
+
         rss_list = Rss.select()
         msg = ""
+        no = 1
         post_url_list = [
             rss_history.url
             for rss_history in History.select().where(
                 History.publish_at == datetime.today().strftime("%Y-%m-%d")
             )
         ]
+
         for rss in rss_list:
             rss_history_list = []
             feed = feedparser.parse(rss.feed)
@@ -43,13 +46,18 @@ class RssRobot:
                     and (datetime.timestamp(datetime.now()) - datetime.timestamp(pub_t))
                     < rss.before * 86400
                 ):
-                    msg = msg + f"{entry.title}\n{entry.link}\n\n"
+                    msg = msg + f"{str(no).zfill(2)}.{entry.title}\n{entry.link}\n\n"
+                    if no % 10 == 0:
+                        send("RSS 订阅", msg)
+                        msg = ""
+                    no += 1
                     rss_history_list.append(History(url=entry.link))
 
             with db.atomic():
                 History.bulk_create(rss_history_list, batch_size=10)
 
-        return msg
+        if no % 10 != 0:
+            send("RSS 订阅", msg)
 
     def remove_old_history(self):
         # 只保留最近一周的记录
@@ -61,4 +69,3 @@ class RssRobot:
 
 if __name__ == "__main__":
     res = RssRobot().main()
-    send("RSS 订阅", res)
