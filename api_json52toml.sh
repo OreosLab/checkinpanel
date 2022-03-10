8f6a1ecc3c458d91b85386202c3bf942e27dbe6f
Oreomeow <85870818+Oreomeow@users.noreply.github.com>
2022-03-11 00:31:25 +0800

    :hammer: 修复重复拉取问题

--- api_json52toml.sh
+++ api_json52toml.sh
@@ -9,7 +9,7 @@ COMMENT
 json52toml() {
     if [ -f "/ql/config/config.sh" ]; then
         sed -i '/^RepoFileExtensions/c RepoFileExtensions="js pl py sh ts"' /ql/config/config.sh
-        ql repo https://github.com/Oreomeow/checkinpanel.git "api_|ck_|ins_" "^checkin" "^notify|^utils" "master"
+        # ql repo https://github.com/Oreomeow/checkinpanel.git "api_|ck_|ins_" "^checkin" "^notify|^utils" "master"
         cp -f /ql/repo/Oreomeow_checkinpanel_master/check.sample.toml /ql/config/check.template.toml
     fi
     find . -type f -name '*utils_json52toml.pl' -exec perl {} \;
