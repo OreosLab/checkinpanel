5512f645f30aa2f55e9b05219e1e0ca9ab6c92c2
night-raise <summerdown@yandex.com>
2022-05-13 00:58:40 +0800

    :bug: 首次运行换源 CPAN 会卡死。
    Signed-off-by: night-raise <summerdown@yandex.com>

--- api_fast_cn.sh
+++ api_fast_cn.sh
@@ -25,7 +25,7 @@ if ! (
         -e 'CPAN::HandleConfig->prettyprint("urllist")' |
         grep -qF 'https://mirrors.tuna.tsinghua.edu.cn/CPAN/'
 ); then
-    perl -MCPAN -e 'CPAN::HandleConfig->load();' \
+    echo "Yes" | perl -MCPAN -e 'CPAN::HandleConfig->load();' \
         -e 'CPAN::HandleConfig->edit("urllist", "unshift", "https://mirrors.tuna.tsinghua.edu.cn/CPAN/");' \
         -e 'CPAN::HandleConfig->commit()'
 fi
