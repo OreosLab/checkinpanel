24dc9e40c02166a3d056e2ff70577c84b5f96e7a
Oreomeow <85870818+Oreomeow@users.noreply.github.com>
2022-05-24 16:39:31 +0800

    :wrench: 增加 elecV2P 任务配置：haidilao

--- ck_haidilao.py
+++ ck_haidilao.py
@@ -5,6 +5,7 @@ new Env('海底捞会员签到');
 """
 
 import json
+
 import requests
 
 from notify_mtr import send
@@ -16,21 +17,23 @@ class Haidilao:
         self.check_items = check_items
 
     @staticmethod
-    def checkin(openid,uid):
+    def checkin(openid, uid):
         url = "https://superapp-public.kiwa-tech.com/"
         headers = {
             "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile MicroMessenger NetType/4G Language/en miniProgram",
             "Content-Type": "json",
-            "AppId": 15
+            "AppId": 15,
         }
         login_data = {
-            "openId" : openid,
-            "country" : "CN",
-            "uid" : uid,
-            "type" : 1,
-            "codeType" : 1
+            "openId": openid,
+            "country": "CN",
+            "uid": uid,
+            "type": 1,
+            "codeType": 1,
         }
-        login = requests.post(url + "login/thirdCommLogin", headers=headers, data=json.dumps(login_data)).text
+        login = requests.post(
+            url + "login/thirdCommLogin", headers=headers, data=json.dumps(login_data)
+        ).text
         try:
             login = json.loads(login)
             if login["success"] != True:
@@ -39,26 +42,44 @@ class Haidilao:
             return "请求失败"
         headers["_HAIDILAO_APP_TOKEN"] = login["data"]["token"]
         headers["ReqType"] = "APPH5"
-        headers["Referer"] = url + "app-sign-in/?SignInToken=" + login["data"]["token"] + "&source=MiniApp"
-        signin = requests.post(url + "activity/wxapp/signin/signin", headers=headers, data=json.dumps({"signinSource" : "MiniApp"})).text
+        headers["Referer"] = (
+            url
+            + "app-sign-in/?SignInToken="
+            + login["data"]["token"]
+            + "&source=MiniApp"
+        )
+        signin = requests.post(
+            url + "activity/wxapp/signin/signin",
+            headers=headers,
+            data=json.dumps({"signinSource": "MiniApp"}),
+        ).text
         try:
             signin = json.loads(signin)
             if signin["success"] != True:
                 return "今日签到过了"
         except json.decoder.JSONDecodeError:
             return "请求失败"
-        fragment = requests.post(url + "activity/wxapp/signin/queryFragment", headers=headers).text
+        fragment = requests.post(
+            url + "activity/wxapp/signin/queryFragment", headers=headers
+        ).text
         try:
             fragment = json.loads(fragment)
             if signin["success"] == True:
-                return "账号：" + login["data"]["name"] + "\n\n签到成功，碎片余额：" + fragment["data"]["total"]
+                return (
+                    "账号："
+                    + login["data"]["name"]
+                    + "\n\n签到成功，碎片余额："
+                    + fragment["data"]["total"]
+                )
         except json.decoder.JSONDecodeError:
             return "请求失败"
 
     def main(self):
         msg_all = ""
         for check_item in self.check_items:
-            msg = self.checkin(openid=str(check_item.get("openid")),uid=str(check_item.get("uid")))
+            msg = self.checkin(
+                openid=str(check_item.get("openid")), uid=str(check_item.get("uid"))
+            )
             msg_all += msg + "\n\n"
         return msg_all
 
