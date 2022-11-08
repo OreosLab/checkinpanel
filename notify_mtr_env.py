6973151fb492854afdacba279bcbead730c730ce
Oreomeow <85870818+Oreomeow@users.noreply.github.com>
2022-11-08 13:36:16 +0800

    :bug: 修复一言异常

--- notify_mtr_env.py
+++ notify_mtr_env.py
@@ -631,7 +631,7 @@ def one() -> str:
         url = "https://v1.hitokoto.cn/"
         res = requests.get(url).json()
         return res["hitokoto"] + "    ----" + res["from"]
-    except requests.exceptions.ConnectionError:
+    except (requests.exceptions.ConnectionError, requests.exceptions.JSONDecodeError):
         return ""
 
 
--- utils_ver.py
+++ utils_ver.py
@@ -5,7 +5,7 @@ import time
 
 import requests
 
-__version__ = "20221030-4-001"
+__version__ = "20221108-4-010"
 ver_re = re.compile("__version__ = .(\\d+-\\d+-...).")
 
 
